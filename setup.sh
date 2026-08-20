f6a943049d7c1abc115b51959473a2f54d5284fa
Hari Kumar <harikumar.codes@gmail.com>
2026-08-20 22:14:48 +0100

    Add RPM Fusion and media codecs

--- setup.sh
+++ setup.sh
@@ -6,3 +6,14 @@ if [[ "$ID" != "fedora" ]]; then
   echo "This script requires Fedora."
   exit 1
 fi
+
+# RPM Fusion
+sudo dnf install -y \
+  "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+  "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
+
+# Media Codecs
+sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing
+sudo dnf -y install @multimedia \
+  --setopt="install_weak_deps=False" \
+  --exclude=PackageKit-gstreamer-plugin
